5992d894461264f0ffb78590577c775678098379
Mainamathenge <mainamathengej@gmail.com>
2023-04-13 14:12:09 +0300

    cart-update

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,7 +6,7 @@ const cartController = require('../controllers/cartController');
 const router = new express.Router();
 
 router.post('/create', cartController.createCart );
-router.post('/delete', cartController.deleteItem);
+router.delete('/delete', cartController.deleteItem);
 // router.post('/forgotPassword', authController.forgotPassword);
 // router.patch('/resetPassword/:token', authController.resetPassword);
 module.exports = router ;
